ed5e6edf9986c050644281c6fdf221d110d07112
Brian Hestir <brhestir@gmail.com>
2021-01-26 15:14:12 -0500

    Updated schema.sql

--- db/schema.sql
+++ db/schema.sql
@@ -1,12 +1,12 @@
 ### Schema
 
-CREATE DATABASE cat_db;
-USE cat_db;
+CREATE DATABASE burgers_db;
+USE burgers_db;
 
-CREATE TABLE cats
+CREATE TABLE burgers
 (
 	id int NOT NULL AUTO_INCREMENT,
-	name varchar(255) NOT NULL,
-	sleepy BOOLEAN DEFAULT false,
+	burger_name varchar(255) NOT NULL,
+	devoured BOOLEAN DEFAULT false,
 	PRIMARY KEY (id)
 );
