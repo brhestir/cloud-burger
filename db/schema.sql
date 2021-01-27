51a0f0baa20f32d4031c414fde4ea14a4ace7dc3
Brian Hestir <brhestir@gmail.com>
2021-01-26 22:13:25 -0500

    Updated schema.sql and seeds.sql for deployment testing

--- db/schema.sql
+++ db/schema.sql
@@ -1,12 +1,7 @@
-### Schema
-
-CREATE DATABASE burgers_db;
-USE burgers_db;
-
-CREATE TABLE burgers
-(
-	id int NOT NULL AUTO_INCREMENT,
+CREATE TABLE burgers (
+	id INT NOT NULL AUTO_INCREMENT,
 	burger_name varchar(255) NOT NULL,
 	devoury BOOLEAN DEFAULT false,
+    -- createdAt TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
 	PRIMARY KEY (id)
 );
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,6 +1,7 @@
-INSERT INTO burgers (burger_name) VALUES ('Western BBQ Bacon');
-INSERT INTO burgers (burger_name) VALUES ('Jalapeno Bidness');
+INSERT INTO burgers (burger_name, devoury) VALUES ('Western BBQ Bacon', false);
+INSERT INTO burgers (burger_name, devoury) VALUES ('Jalapeno Bidness', false);
 INSERT INTO burgers (burger_name, devoury) VALUES ('Bleu Cheese Crumble', true);
 INSERT INTO burgers (burger_name, devoury) VALUES ('Chonk', true);
 INSERT INTO burgers (burger_name, devoury) VALUES ('Vegan', true);
-INSERT INTO burgers (burger_name) VALUES ('Haute Not');
+INSERT INTO burgers (burger_name, devoury) VALUES ('Haute Not', true);
+INSERT INTO burgers (burger_name, devoury) VALUES ('Wow burger', true);
