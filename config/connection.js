440d9bf1dbfd613ab380b3a7854402e5cf485e60
Brian Hestir <brhestir@gmail.com>
2021-01-26 21:25:49 -0500

    Updating connection.js details for cloud deployment

--- config/connection.js
+++ config/connection.js
@@ -1,15 +1,23 @@
 // Set up MySQL connection.
 var mysql = require("mysql");
 
-var connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "toor!TOOR!",
-  database: "burgers_db"
-});
+var connection;
 
-// Make connection.
+
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+}
+else {
+  connection = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "hacktheplanet",
+    database: "burgers_db"
+  });
+}
+  
+
+// Make connection locally
 connection.connect(function(err) {
   if (err) {
     console.error("error connecting: " + err.stack);
